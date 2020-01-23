4f9cb648483bc8aa97937ef21fac5fa59c8e0902
Manuel Larios <mlarios12@wou.edu>
2020-01-22 23:05:46 -0800

    Finished final design and added seekbar functionality

--- Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,11 +2,30 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var slider : SeekBar
+    lateinit var value : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        slider = findViewById(R.id.peopleSlider) as SeekBar
+        value = findViewById(R.id.textPeopleValue) as TextView
+
+        slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                value.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
     }
 }
