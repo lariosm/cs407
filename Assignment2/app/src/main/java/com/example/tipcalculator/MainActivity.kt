5bfa3cd88e961af0f6213d79501eb3325681a0f8
Manuel Larios <mlarios12@wou.edu>
2020-01-23 23:06:04 -0800

    Finished adding all functionality needed to work properly

--- Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.tipcalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
 import android.widget.SeekBar
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+
+const val HUNDRED_PERCENT = 100.00
 
 class MainActivity : AppCompatActivity() {
     lateinit var slider : SeekBar
@@ -16,9 +22,50 @@ class MainActivity : AppCompatActivity() {
         slider = findViewById(R.id.peopleSlider) as SeekBar
         value = findViewById(R.id.textPeopleValue) as TextView
 
+        fun calculateExpense() {
+            val totalAmountDue : Double = if(editBillInput.text.isNotEmpty()) editBillInput.text.toString().toDouble() else 24.98
+            val percentToTip = if(editTipInput.text.isNotEmpty()) editTipInput.text.toString().toInt() else 15
+            val peopleCount = if(value.text.isNotEmpty()) value.text.toString().toInt() else 1
+
+            val amtToTip : Double = (totalAmountDue * (percentToTip / HUNDRED_PERCENT))
+            val totalExpense : Double = (totalAmountDue * (percentToTip / HUNDRED_PERCENT)) + totalAmountDue
+            val perPersonTotal = totalExpense / peopleCount
+
+            textTipValue.text = String.format("$%.2f", amtToTip)
+            textBillValue.text = String.format("$%.2f", totalExpense)
+            textPerPersonValue.text = String.format("$%.2f", perPersonTotal)
+        }
+
+        editBillInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateExpense()
+            }
+
+        })
+
+        editTipInput.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                calculateExpense()
+            }
+
+        })
+
         slider.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
-                value.text = progress.toString()
+                value.text = (progress + 1).toString()
+                calculateExpense()
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
