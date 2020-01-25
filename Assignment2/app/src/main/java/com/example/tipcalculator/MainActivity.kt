ec8c9e8cdfd6025f54fdd6747b9814e941fc7e6b
Manuel Larios <mlarios12@wou.edu>
2020-01-24 22:45:57 -0800

    Decoupled calculateExpense() for unit testing

--- Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
+++ Assignment2/app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -26,18 +26,24 @@ class MainActivity : AppCompatActivity() {
         val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, tipList)
         var tipSelected = ""
 
+        fun expenseCruncher(amountDue : Double, percentTip: Int, numPeople : Int) : Triple<Double, Double, Double> {
+            val amtToTip : Double = amountDue * (percentTip / HUNDRED_PERCENT)
+            val totalExpense  = amtToTip + amountDue
+            val perPersonTotal = totalExpense / numPeople
+
+            return Triple(amtToTip, totalExpense, perPersonTotal)
+        }
+
         fun calculateExpense() {
             val totalAmountDue : Double = if(editBillInput.text.isNotEmpty()) editBillInput.text.toString().toDouble() else 24.98
             val percentToTip = if(tipSelected.isNotEmpty()) tipSelected.toInt() else 5
             val peopleCount = if(value.text.isNotEmpty()) value.text.toString().toInt() else 1
 
-            val amtToTip : Double = totalAmountDue * (percentToTip / HUNDRED_PERCENT)
-            val totalExpense  = amtToTip + totalAmountDue
-            val perPersonTotal = totalExpense / peopleCount
+            val (tipAmount, totalAmount, splitAmount) = expenseCruncher(totalAmountDue, percentToTip, peopleCount)
 
-            textTipValue.text = String.format("$%.2f", amtToTip)
-            textBillValue.text = String.format("$%.2f", totalExpense)
-            textPerPersonValue.text = String.format("$%.2f", perPersonTotal)
+            textTipValue.text = String.format("$%.2f", tipAmount)
+            textBillValue.text = String.format("$%.2f", totalAmount)
+            textPerPersonValue.text = String.format("$%.2f", splitAmount)
         }
 
         editBillInput.addTextChangedListener(object : TextWatcher {
