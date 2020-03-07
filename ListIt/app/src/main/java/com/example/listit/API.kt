b98d6e30702d4234f0c20b5dfce2ac264c84008e
Manuel Larios <mlarios12@wou.edu>
2020-03-07 00:19:03 -0800

    Added interface class for querying API

---
+++ ListIt/app/src/main/java/com/example/listit/API.kt
@@ -0,0 +1,13 @@
+package com.example.listit
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface API {
+    @GET("api/listings")
+    fun getBusinessResults(
+        @Query("limit") results: Int = 20,
+        @Query("offset") offset: Int
+    ): Call<GetListingResponse>
+}
